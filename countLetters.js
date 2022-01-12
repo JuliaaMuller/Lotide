2a2a23858e859e0c4ca414a4997ede1d818d3177
JuliaaMuller <julialm.muller@gmail.com>
2022-01-12 14:52:14 -0500

    countletters exercice

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(strings) {
+  const results = {}
+  for (const letters of strings) {
+    if (results[letters]) {
+      results[letters] += 1;
+    } else {
+      results[letters] = 1;
+    }
+  }
+  return results
+  }
+  
+console.log(countLetters("lighthouse in the house"))
