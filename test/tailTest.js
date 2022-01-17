56e6eff6249421c85cab87f960d61579a615ed53
JuliaaMuller <julialm.muller@gmail.com>
2022-01-17 14:49:26 -0500

    refactor woth mocha and chai I

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,16 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+// const assertArraysEqual = require('../assertArraysEqual');
 const tail = require('../tail');
-const words = ["Yo Yo", "Lighthouse", "Labs"];
 
-assertArraysEqual(tail(words), ["Lighthouse", "Labs"]); 
+describe("#tail", () => {
+  it("returns [2] for [1, 2]", () => {
+    assert.deepEqual(tail([1, 2]), [2]);
+  });
+  it("returns [2, 3, 4, 5] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]); 
+  });
+  it('returns ["Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]); 
+  });
+});
+
