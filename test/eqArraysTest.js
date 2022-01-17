72d74438c8defe1430333ea3fc65aec04f2704df
JuliaaMuller <julialm.muller@gmail.com>
2022-01-17 13:48:46 -0500

    all modification

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -3,5 +3,5 @@ const eqArrays = require('../eqArrays');
 
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
