b232ff497c48d6f7ff8a0ffdcf7bb6f88ba054b3
JuliaaMuller <julialm.muller@gmail.com>
2022-01-17 19:40:18 -0500

    last change

--- head.js
+++ head.js
@@ -1,9 +1,5 @@
-const head = function(array, result) {
-  let final;
-  if (result = array[0]) {
-    final = result;
-  }
-  return final;
+const head = function(array) {
+  return array[0];
 };
 
 
